01231b99e88a625c62f967cd2fe75431f42a8956
Jeferson Oliveira <zuruckyt@gmail.com>
2022-09-09 23:01:29 -0300

    feat: use Pokemon type

--- src/components/PokeCard/index.tsx
+++ src/components/PokeCard/index.tsx
@@ -2,21 +2,23 @@ import { View, Text, Image } from "react-native";
 import { styles } from "./styles";
 import { colors } from "../../global/styles/colors";
 
+type PokeCardProps = {
+    pokemon: Pokemon;
+}
 
-
-export function PokeCard({ id, name, type, image }: Pokemon ) {
-    const typeColor = colors.types[type || "normal"];
+export function PokeCard({ pokemon }: PokeCardProps  ) {
+    const typeColor = colors.types[pokemon.types[0].type.name || "normal"];
 
     return (
         <View style={[styles.card, {borderColor: typeColor}]}>
             <View style={styles.pokemonIdContainer}>
-                <Text style={[styles.pokemonIdText, {color: typeColor}]}>#{id}</Text>
+                <Text style={[styles.pokemonIdText, {color: typeColor}]}>#{pokemon.id}</Text>
             </View>
             <View style={styles.pokemonImageContainer}>
-                <Image style={styles.pokemonImage} source={{uri: image}}/>
+                <Image style={styles.pokemonImage} source={{uri: pokemon.sprites.other['official-artwork'].front_default}}/>
             </View>
             <View style={[styles.pokemonNameContainer, {backgroundColor: typeColor}]}>
-                <Text style={styles.pokemonNameText}>{name}</Text>
+                <Text style={styles.pokemonNameText}>{pokemon.name}</Text>
             </View>
         </View>
     );
