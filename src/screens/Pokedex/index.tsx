aa0cc541f65bf4a8c2bd527d0c20be914a55b323
0horaa <osergiogabriel12@gmail.com>
2022-10-04 09:31:38 -0300

    refactor: separing styles in another file

--- src/screens/Pokedex/index.tsx
+++ src/screens/Pokedex/index.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, StatusBar, Dimensions, FlatList, ActivityIndicator } from 'react-native';
+import { Text, TextInput, View, FlatList, ActivityIndicator } from 'react-native';
+
 import { Pokemon } from '../../@types';
 import { PokeCard } from '../../components/PokeCard';
+
+import { styles } from "./styles";
 
 type Props = {
     pokemons: Pokemon[];
@@ -9,8 +12,7 @@ type Props = {
 }
 
 export function Pokedex({ pokemons, isLoading }: Props) {
-
-    const [search, setSearch] = useState<string>();
+    const [search, setSearch] = useState("");
 
     return (
         <View style={styles.container}>
@@ -37,76 +39,3 @@ export function Pokedex({ pokemons, isLoading }: Props) {
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f7f7f7',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-        paddingTop: StatusBar.currentHeight,
-        maxHeight: Dimensions.get('screen').height
-    },
-    navbar: {
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    },
-    brand: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    logo: {
-        marginRight: 10,
-        width: 50,
-        height: 50,
-        backgroundColor: '#000',
-    },
-    title: {
-        fontFamily: 'Poppins-Bold',
-        color: '#212121',
-        fontSize: 40,
-    },
-    controlsContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        height: 50,
-    },
-    controlsText: {
-        fontSize: 25,
-        marginRight: 5
-    },
-    controlsIcon: {
-        width: 15,
-        height: 30,
-        backgroundColor: '#000'
-    },
-    searchContainer: {
-        marginTop: 20,
-        width: '100%',
-    },
-    searchInput: {
-        height: 40,
-        borderRadius: 20,
-        fontSize: 20,
-        textAlign: 'center',
-        fontFamily: 'Poppins-Regular',
-        textAlignVertical: 'center',
-        paddingHorizontal: 20,
-        backgroundColor: '#fff',
-    },
-    scrollContainer: {
-        marginTop: 10,
-        flex: 1,
-        width: '100%',
-        justifyContent: "center"
-    },
-    cardContainer: {
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        justifyContent: 'space-between',
-        flexWrap: 'wrap',
-    },
-});
---
+++ src/screens/Pokedex/styles.ts
@@ -0,0 +1,74 @@
+import { StyleSheet, StatusBar, Dimensions } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f7f7f7',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingTop: StatusBar.currentHeight,
+        maxHeight: Dimensions.get('screen').height
+    },
+    navbar: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    brand: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    logo: {
+        marginRight: 10,
+        width: 50,
+        height: 50,
+        backgroundColor: '#000',
+    },
+    title: {
+        fontFamily: 'Poppins-Bold',
+        color: '#212121',
+        fontSize: 40,
+    },
+    controlsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 50,
+    },
+    controlsText: {
+        fontSize: 25,
+        marginRight: 5
+    },
+    controlsIcon: {
+        width: 15,
+        height: 30,
+        backgroundColor: '#000'
+    },
+    searchContainer: {
+        marginTop: 20,
+        width: '100%',
+    },
+    searchInput: {
+        height: 40,
+        borderRadius: 20,
+        fontSize: 20,
+        textAlign: 'center',
+        fontFamily: 'Poppins-Regular',
+        textAlignVertical: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: '#fff',
+    },
+    scrollContainer: {
+        marginTop: 10,
+        flex: 1,
+        width: '100%',
+        justifyContent: "center"
+    },
+    cardContainer: {
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+    },
+});
